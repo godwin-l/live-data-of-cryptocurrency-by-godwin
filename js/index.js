b10e4b36080195967f16db0b50ee89133cfea32d
alloygodwin1 <34455677+alloygodwin1@users.noreply.github.com>
2018-07-31 21:57:27 +0530

    live data implemented
    by godwin

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+var app=angular.module('formvalid', ['ui.bootstrap','ui.utils']);
+app.controller('validationCtrl',function($scope){
+  $scope.data=[
+        
+    ]
+
+
+$scope.dataTableOpt = {
+
+  "aLengthMenu": [[10, 50, 100,-1], [10, 50, 100,'All']],
+  };
+});
